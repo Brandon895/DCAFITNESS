a117d59d3b72daeb59acea02d129e59ae6a706ad
Brandon895 <brandonzuniga3320@gmail.com>
2025-04-27 23:41:37 -0600

    Update BitacoraModel.php

--- models/BitacoraModel.php
+++ models/BitacoraModel.php
@@ -3,8 +3,22 @@ class BitacoraModel {
     private $db;
 
     public function __construct() {
-        $this->db = new mysqli("localhost", "root", "0895Gazuniga", "dcafitness");
+        // Obtener la URL de la base de datos desde la variable de entorno
+        $database_url = getenv('DATABASE_URL');
 
+        // Parsear la URL para obtener los detalles de la conexión
+        $parsed_url = parse_url($database_url);
+
+        // Extraer los datos de la URL
+        $servername = $parsed_url['host'];      // sql206.infinityfree.com
+        $username = $parsed_url['user'];       // if0_38849919
+        $password = $parsed_url['pass'];       // 0895Gazuniga
+        $database = ltrim($parsed_url['path'], '/'); // if0_38849919_XXX
+
+        // Establecer la conexión
+        $this->db = new mysqli($servername, $username, $password, $database);
+
+        // Verificar la conexión
         if ($this->db->connect_error) {
             die("Error en la conexión: " . $this->db->connect_error);
         }

0286205ec5b1164abd36364d07e97533a2d7185d
Brandon895 <brandonzuniga3320@gmail.com>
2025-04-29 20:33:14 -0600

    Update db.php

--- models/db.php
+++ models/db.php
@@ -1,20 +1,10 @@
 <?php
-$databaseUrl = getenv('DATABASE_URL');
+$databaseUrl = getenv('DATABASE_URL'); // Obtiene la variable de entorno
+$dsn = parse_url($databaseUrl); // Parseamos la URL
+$dbConnection = new mysqli($dsn['host'], $dsn['user'], $dsn['pass'], ltrim($dsn['path'], '/'));
 
-// Convertir la URL en partes
-$components = parse_url($databaseUrl);
-
-$servername = $components['host'];
-$username   = $components['user'];
-$password   = $components['pass'];
-$database   = ltrim($components['path'], '/');
-$port       = $components['port'];
-
-// Crear la conexión usando las variables correctas
-$conn = new mysqli($servername, $username, $password, $database, $port);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+if ($dbConnection->connect_error) {
+    die("Connection failed: " . $dbConnection->connect_error);
 }
+echo "Connected successfully!";
 ?>
